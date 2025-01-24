14077e19b0e37269eb806ccde134f11d8eb67b69
Nadir123649 <nadir.ali@novatoresols.com>
2025-01-24 22:07:52 +0500

    set the contact is form section

---
+++ src/components/CigmaSection.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const CigmaSection = () => {
+    return (
+        <section>
+            <Container className='cigma-container'>
+                <Row>
+                    <Col lg={6} md={12} xs={12}>
+                        <div>
+                            <h1>
+                                How Cigma Helps You Stay
+                                <span>
+                                    Connected
+                                </span>
+                                and
+                                <span>
+                                    Healthy
+                                </span>
+                            </h1>
+                            <div className='cigma-card'>
+                                <div>
+                                    <p>
+                                        How It Works
+                                    </p>
+                                </div>
+                                <h3>
+                                    Your Path to Wellness
+                                </h3>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col lg={6} md={12} xs={12}>
+                        <p>
+                            Our features are built around fostering better mental health for all.
+                        </p>
+                        <div>
+                            <ul>
+                                <li>
+                                    <div>
+                                        01
+                                    </div>
+                                    <img src="" alt="" />
+                                    <span>
+                                        Create an Account
+                                    </span>
+                                </li>
+                                <li>
+                                    <div>
+                                        02
+                                    </div>
+                                    <img src="" alt="" />
+                                    <span>
+                                        Start Conversations With AI
+                                    </span>
+                                </li>
+                                <li>
+                                    <div>
+                                        03
+                                    </div>
+                                    <img src="" alt="" />
+                                    <span>
+                                        Track Your Wellness
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
+
+export default CigmaSection;
