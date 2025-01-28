48635f5b5fd754ded467f43f4dbfe34f67969fd5
Maham Rana <maham.rana@novatoresols.com>
2025-01-28 13:24:08 +0500

    add recommended slider

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -1,10 +1,12 @@
 import FAQ from '@/components/FAQ';
 import Layout from '@/components/layout';
+import RecommendedSlider from '@/components/RecommendedSlider';
 
 const Resources = () => {
 
     return (
         <Layout showContactForm={true}>
+            <RecommendedSlider/>
             <FAQ/>
         </Layout>
     );
