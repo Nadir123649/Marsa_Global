d931ab874a6198c99331fc9eb04b013f7d918462
Maham Rana <maham.rana@novatoresols.com>
2025-01-30 19:44:05 +0500

    add video

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,6 +8,8 @@ import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter();
+    const [videoWatched, setVideoWatched] = useState(false); // State to track video watched
+
     useEffect(() => {
         if ('scrollRestoration' in window.history) {
             window.history.scrollRestoration = 'manual';
@@ -21,10 +23,33 @@ function MyApp({ Component, pageProps }) {
             router.events.off('routeChangeComplete', handleRouteChange);
         };
     }, [router.events]);
+
+    const handleVideoEnd = () => {
+        setVideoWatched(true);
+    };
+
     return (
         <>
-            <Component {...pageProps} />
+            {!videoWatched ? (
+                <div className='h-[100vh] relative'>
+                    <video autoPlay width="100%" height="100vh" onEnded={handleVideoEnd} controls>
+                    <source src="/images/video-intro.mp4"  type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    
+                    <button 
+                    className='video-btn'
+                        onClick={() => setVideoWatched(true)} 
+                        style={{ display: 'block', margin: '20px auto' }}
+                    >
+                       Skip Introduction
+                    </button>
+                </div>
+            ) : (
+                <Component {...pageProps} />
+            )}
         </>
     );
 }
+
 export default MyApp;
